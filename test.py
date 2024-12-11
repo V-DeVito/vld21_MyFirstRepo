1aef09ee8f917e10c22ef8f1e2537d734b25bea8
Vicenzo DeVito <13685088+V-DeVito@users.noreply.github.com>
2024-12-10 20:40:47 -0500

    Update test.py
    Add shimmer.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import math
 
 vertices = (
     (1, -1, -1),
@@ -21,7 +22,6 @@ edges = (
     (0, 4), (1, 5), (2, 6), (3, 7)
 )
 
-
 def draw_cube():
     glBegin(GL_LINES)
     for edge in edges:
@@ -29,16 +29,16 @@ def draw_cube():
             glVertex3fv(vertices[vertex])
     glEnd()
 
-
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-
+    
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -5)
 
     clock = pygame.time.Clock()
+    shimmer_angle = 0
 
     while True:
         for event in pygame.event.get():
@@ -46,12 +46,21 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        shimmer_angle += 1
+        shimmer_factor = (math.sin(math.radians(shimmer_angle)) + 1) / 2  # Varies between 0 and 1
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()
+        glRotatef(1, 3, 1, 1)
+        glColor4f(1.0, 1.0, 1.0, shimmer_factor)  # Adjust alpha based on shimmer_factor
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         draw_cube()
+        glDisable(GL_BLEND)
+        glPopMatrix()
+        
         pygame.display.flip()
         clock.tick(60)
-
 
 if __name__ == "__main__":
     main()
